a27a6e3d8beca7088686b396a369c82c417b0459
Felipe Batista <felipeb2silva@gmail.com>
2021-09-01 02:46:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -11,7 +11,13 @@ Label(janela, text='Qual o ângulo?').grid(row=0, column=0)
 texto_pergunta = Entry(janela).grid(row=0, column=1,)
 Btn = Button(janela, text="calcular").grid(column=1,row=3)
 
- 
+ang = float(input('Qual o angulo ? ',))
+seno = sin(radians(ang))
+print('Angulo {} tem seno {:.2f}'.format(ang,seno))
+coss = cos(radians(ang))
+print('Angulo {} tem cosseno {:.2f}'.format(ang,coss))
+tang = tan(radians(ang))
+print('Angulo {} tem tangente {:.2f}'.format(ang,tang))
 
 
 janela.mainloop()
